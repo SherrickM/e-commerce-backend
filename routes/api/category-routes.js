dd1e6445b19776f3dbf22281fc834081248aa49b
Marquis <smcneal89@gmail.com>
2022-08-04 19:31:08 -0400

    updated category and product

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -26,20 +26,17 @@ router.get("/:id", (req, res) => {
 });
 
 router.post("/", async (req, res) => {
-  try {
-    const categoryData = await Category.create({
-      reader_id: req.body.reader_id,
-    });
+   try {
+    const categoryData = await Category.create(req.body);
     res.status(200).json(categoryData);
   } catch (err) {
-    res.status(400).json(err);
+    res.status(500).json(err);
   }
 });
 
 router.put("/:id", async (req, res) => {
   // update a category by its `id` value
-  
-  try {
+    try {
     const categoryData = await Category.update(req.body, {
         where: {
             id: req.params.id
@@ -60,7 +57,7 @@ router.delete("/:id", (req, res) => {
     .then((results) => {
       console.log(results);
       if (results === 0) {
-        res.status(400).json({ message: "no changes made..." });
+        res.status(400).json({ message: "no changes made." });
       } else {
         res.json(results);
       }
